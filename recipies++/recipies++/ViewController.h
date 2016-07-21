7a397b02edb49fdcde4da7ecb93e78b0da29c5a5
Thomas Minshull <thomasminshull@gmail.com>
2016-07-21 11:39:19 -0700

    initial commit

---
+++ recipies++/recipies++/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  recipies++
+//
+//  Created by thomas minshull on 2016-07-21.
+//  Copyright © 2016 thomas minshull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ recipies++/recipies++/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  recipies++
+//
+//  Created by thomas minshull on 2016-07-21.
+//  Copyright © 2016 thomas minshull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
